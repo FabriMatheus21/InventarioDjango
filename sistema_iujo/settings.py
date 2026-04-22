58d0a17bd8efeb734084862645648e53a9bf00f4
FabriMatheus21 <fabriciom2106@gmail.com>
2026-04-22 15:57:37 -0400

    definicion de modelo de datos

--- sistema_iujo/settings.py
+++ sistema_iujo/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inventario',  
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -115,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Configuración de Redirección (Seguridad) 
+LOGIN_REDIRECT_URL = 'lista_productos' 
+LOGOUT_REDIRECT_URL = 'login' 
